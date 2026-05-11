636fb7921aef34e64da93e3e83be78532ea867ea
JoDauto <josebolinijr@gmail.com>
2026-05-11 19:17:35 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 import java.time.LocalDate;
@@ -13,6 +15,8 @@ public class Program {
         System.out.println(department);
 
         Seller seller = new Seller(21, "Jose", "jose@gmail.com", LocalDate.now(), 2000.0, department);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(seller);
     }
