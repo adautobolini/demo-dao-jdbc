d093c0110079bf1c1448e28f6f48ed4517468472
JoDauto <josebolinijr@gmail.com>
2026-05-13 19:05:11 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,11 +35,14 @@ public class Program {
         System.out.println("Inserted! New id = " + newSeller.getId());
 
         System.out.println("\n=== TEST 5: seller update ===");
-        seller = sellerDao.findById(2);
+        seller = sellerDao.findById(3);
         seller.setName("Jeremias");
         sellerDao.update(seller);
         System.out.println(seller);
 
+        System.out.println("\n=== TEST 6: seller delete ===");
+        sellerDao.deleteById(10);
+        System.out.println("Delete completed");
 
     }
 }
