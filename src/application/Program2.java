6697a9cfdd88e3eb3f38c18fd876829a9afeb229
JoDauto <josebolinijr@gmail.com>
2026-05-14 19:45:54 -0300

    insert and update implementation 2

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -16,15 +16,26 @@ public class Program2 {
         Department dep = departmentDao.findById(2);
         System.out.println(dep);
 
-        System.out.println("\n=== TEST 2: department findAll ===");
+        System.out.println("\n=== TEST 2: department findAllWithOrder ===");
         List<Department> list = departmentDao.findAllWithOrder("Id");
 
         for(Department dep1 : list){
             System.out.println(dep1);
         }
 
+        System.out.println("\n=== TEST 3: department insert ===");
+        Department dep2 = new Department(null,"Kitchen");
+        departmentDao.insert(dep2);
+        System.out.println("INSERTED!");
 
-
+        System.out.println("\n=== TEST 4: department update ===");
+        Department dep3 = departmentDao.findById(1);
+        System.out.println(dep3);
+        dep3.setName("Drivers");
+        departmentDao.update(dep3);
+        System.out.println("Updated!");
+        dep3 = departmentDao.findById(1);
+        System.out.println(dep3);
 
 
 
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -5,10 +5,7 @@ import db.DbException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,11 +19,46 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
     @Override
     public void insert(Department obj) {
+        PreparedStatement st = null;
+        try{
+            st = conn.prepareStatement("INSERT INTO department (Name) VALUES (?);");
+
+            st.setString(1, obj.getName());
+
+            st.executeUpdate();
+
+        }
+        catch (SQLException e){
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+        }
 
     }
 
     @Override
     public void update(Department obj) {
+
+        PreparedStatement st = null;
+        try{
+            st = conn.prepareStatement("UPDATE department SET Name = ? WHERE Id = ?;");
+
+            st.setString(1, obj.getName());
+            st.setInt(2, obj.getId());
+
+            int rowsAffected = st.executeUpdate();
+
+            if(rowsAffected == 0){
+                System.out.println("Id Inexistente!");
+            }
+        }
+        catch(SQLException e){
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+        }
 
     }
 
